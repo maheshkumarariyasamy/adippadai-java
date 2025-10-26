d7bd8544168bb9f71c61b53bf23bab55ba432bbb
maheshkumarariyasamy <maheshkumar.ariyasamy@gmail.com>
2025-10-26 21:45:05 +0530

    Arrays Initialization types

---
+++ src/nall/ondru/arrays/ArrayUtils.java
@@ -0,0 +1,18 @@
+package nall.ondru.arrays;
+
+public class ArrayUtils {
+    // Array - Store multiple values in  single variable
+
+    //Array Initialization - Type 1
+    char[] vowels = {'A','E','I','O','U'};
+
+    //Array Declaration - Type 2
+    String[] passwords = new String[10];  // each element of the array is initialised to 0
+
+
+    byte[] digits = new byte[128];    //digit[] = new byte[array size];
+                                     //new keyword allocate the memory in heap memory during compile time
+
+    //Array Declaration - Type 3
+    int[] serialNumbers = new int[]{26565,3223,3244,12121};
+}
